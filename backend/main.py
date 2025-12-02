307b281ee84bbba068b0043c5b7c40c8e0bf9f29
Billy Wu <biyi.wu@gmail.com>
2025-12-02 22:39:18 +1100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,8 @@ from pwdlib import PasswordHash
 
 app = FastAPI()
 
+pwd_context = PasswordHash.recommended()
+
 class User(BaseModel):
     id: int
     username: str
@@ -33,6 +35,9 @@ def signup(user: User):
         elif u.email == user.email:
             raise HTTPException(status_code=400, detail="ERROR: Email " \
             "   already exists!")
+    hashed_password = pwd_context.hash(user.password)
+    user.password = hashed_password
+
     users.append(user)
     return {"message": "User signed up successfully", "user": user}
 
@@ -42,11 +47,15 @@ class LoginRequest(BaseModel):
 
 @app.post("/users/login")
 def login(request: LoginRequest):
-    for u in users:
-        if u.username == request.username and u.password == request.password:
-            return {"message": f"Login successful for {u.username}"}
-    raise HTTPException(status_code=401, detail="ERROR: Invalid username "
-        "or password")
+    for user in users:
+        if user.username == request.username:
+            if pwd_context.verify(request.password, user.password):
+                return {"message": f"Login successful for {u.username}"}
+            else:
+                raise HTTPException(status_code=401, detail="ERROR: Invalid " \
+                "password")
+    raise HTTPException(status_code=401, detail="ERROR: Invalid username or " \
+    "password")
 
 @app.get("/workouts")
 def get_workouts(user_id: int = Query(..., description="User ID")):
@@ -55,8 +64,9 @@ def get_workouts(user_id: int = Query(..., description="User ID")):
 
 @app.post("/workouts")
 def add_workout(workout: Workout):
-    if not any(u.id == workout.user_id for u in users):
-        raise HTTPException(status_code=400, detail="ERROR: User does not exist")
+    if not any(user.id == workout.user_id for user in users):
+        raise HTTPException(status_code=400, detail="ERROR: " \
+        "User does not exist")
     workouts.append(workout)
     return workout
 
@@ -65,7 +75,8 @@ def update_workout(workout_id: int, updated: Workout):
     for i, workout in enumerate(workouts):
         if workout.id == workout_id:
             if not any(u.id == updated.user_id for u in users):
-                raise HTTPException(status_code=400, detail="ERROR: User does not exist")
+                raise HTTPException(status_code=400, detail="ERROR: " \
+                "User does not exist")
             workouts[i] = updated
             return updated
     raise HTTPException(status_code=404, detail="ERROR: Workout not found")
